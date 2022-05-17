27896133c4d9c5f5acb090a97713964dfe5c0a2e
Francisco.Javier.Rosa <Francisco.Javier.Rosa.Rodriguez@ibm.com>
2022-05-17 13:18:52 +0200

    Rama form iniciada

--- 02-lazy/react-adv/src/01-lazyload/pages/NoLazy.tsx
+++
@@ -1,3 +0,0 @@
-export const NoLazy = () => {
-  return <div>NoLazy</div>;
-};
--- 02-lazy/react-adv/src/routes/routes.ts
+++ 02-lazy/react-adv/src/routes/routes.ts
@@ -1,6 +1,3 @@
-import { lazy } from 'react';
-import { ShoppingPage } from '../02-component-patterns/pages/ShoppingPage';
-
 type JSXComponent = () => JSX.Element;
 
 interface Route {
@@ -9,16 +6,4 @@ interface Route {
   Component: React.LazyExoticComponent<JSXComponent> | JSXComponent;
   name: string;
 }
-
-const Lazy1 = lazy(() => import('../01-lazyload/pages/LazyPage1'));
-const Lazy2 = lazy(() => import('../01-lazyload/pages/LazyPage2'));
-const Lazy3 = lazy(() => import('../01-lazyload/pages/LazyPage3'));
-
-export const routes: Route[] = [
-  {
-    path: '/shopping',
-    to: 'shopping',
-    Component: ShoppingPage,
-    name: 'ShoppingPage',
-  },
-];
+export const routes: Route[] = [];
