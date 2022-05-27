f99c365c105d838c753c22b5a5c7109006ee99b0
Francisco.Javier.Rosa <Francisco.Javier.Rosa.Rodriguez@ibm.com>
2022-05-27 13:40:30 +0200

    Finished workbox

--- react-calendar-frontend/src/sw-template.js
+++ react-calendar-frontend/src/sw-template.js
@@ -8,21 +8,22 @@ const { registerRoute } = workbox.routing;
 const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
 const { BackgroundSyncPlugin } = workbox.backgroundSync;
 
-registerRoute(new RegExp('http://localhost:4000/api/auth/renew'), new NetworkFirst());
+const cacheNetworkFirst = ['/api/auth/renew', '/api/events'];
 
-registerRoute(new RegExp('http://localhost:4000/api/events'), new NetworkFirst());
+registerRoute(({ request, url }) => {
+  if (cacheNetworkFirst.includes(url.pathname)) return true;
+  return false;
+}, new NetworkFirst());
 
-registerRoute(
-  new RegExp('https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'),
-  new CacheFirst()
-);
+const cachesFirst = [
+  'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css',
+];
 
-registerRoute(
-  new RegExp(
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'
-  ),
-  new CacheFirst()
-);
+registerRoute(({ request, url }) => {
+  if (cachesFirst.includes(url.href)) return true;
+  return false;
+}, new CacheFirst());
 
 const bgSyncPlugin = new BackgroundSyncPlugin('posteos-offline', {
   maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
